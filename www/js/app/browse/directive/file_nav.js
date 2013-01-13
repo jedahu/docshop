dda0b215cadf9042e60cbbbb5941dc753bacb01c
Jeremy Hughes <jedahu@gmail.com>
2013-01-14 11:26:07 +1300

    better file_nav behaviour

--- www/js/app/browse/directive/file_nav.js
+++ www/js/app/browse/directive/file_nav.js
@@ -2,14 +2,16 @@ define(function()
 
 { var fileNavDirective = function($compile)
   { return function fileNav(scope, elem, attr)
-    { var headingTemplate = elem[0].firstElementChild
-        , fileTemplate = elem[0].lastElementChild
+    { var headingTemplate = angular.element(elem[0].firstElementChild)
+        , fileTemplate = angular.element(elem[0].lastElementChild)
         , parts = attr.dsFileNav.split(/\s+/)
         , varname = parts[0]
         , tree = parts[2]
         , item
         , i
         , sublist
+        , subitems
+        , index
         , heading
         , headingText
         , file
@@ -19,32 +21,53 @@ define(function()
     ; scope.$watch
         ( tree
         , function(value)
-          { elem[0].innerHTML = ''
+          { elem.html('')
           ; (function populate(parent, items)
             { for (i in items)
               { item = items[i]
+              ; if (item.name === '!index') continue
               ; newScope = scope._wrapperScope.$new()
               ; if (Object.keys(item).length === 1) // sublist
-                { sublist = document.createElement(elem[0].tagName)
-                ; heading = headingTemplate.cloneNode(true)
+                { sublist = angular.element(document.createElement(elem[0].tagName))
+                ; heading = headingTemplate.clone(true)
                 ; headingText = Object.keys(item)[0]
-                ; sublist.appendChild(heading)
-                ; parent.appendChild(sublist)
-                ; newScope[varname] = {text: headingText}
+                ; sublist.append(heading)
+                ; parent.append(sublist)
+                ; subitems = item[headingText]
+                ; if ((index = subitems.filter
+                        ( function(x)
+                          { return x.name === '!index'
+                          }
+                        )
+                      )
+                      . length > 0)
+                  { newScope[varname] = index[0]
+                  ; heading.data('path', index[0].path)
+                  }
+                  else newScope[varname] = {}
+                ; newScope[varname].text = headingText
                 ; $compile(heading)(newScope)
-                ; populate(sublist, item[headingText])
+                ; populate(sublist, subitems)
                 }
                 else // file
-                { file = fileTemplate.cloneNode(true)
-                ; parent.appendChild(file)
+                { file = fileTemplate.clone(true)
+                ; parent.append(file)
                 ; newScope[varname] = item
+                ; file.data('path', item.path)
                 ; $compile(file)(newScope)
                 }
               }
-            })(elem[0], value)
+            })(elem, value)
           }
         )
-    //; $compile(elem.contents())(scope)
+    ; elem.bind
+        ( 'click'
+        , function(evt)
+          { var elem = angular.element(evt.target)
+              , path = elem.data('path')
+          ; if (path) scope.$apply(function() { scope.changePathTo(path) })
+          }
+        )
     }
   }
 
