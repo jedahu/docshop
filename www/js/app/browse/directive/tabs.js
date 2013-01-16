1402fbb370bd427ece5245017e76104b579b2d5e
Jeremy Hughes <jedahu@gmail.com>
2013-01-16 17:58:16 +1300

    remove print statement

--- www/js/app/browse/directive/tabs.js
+++ www/js/app/browse/directive/tabs.js
@@ -2,7 +2,7 @@ define(function()
 
 { var tabsDirective = function()
   { return function tabs(scope, elem, attr)
-    { console.log(scope.$eval(attr.dsTabs));elem.tabs(scope.$eval(attr.dsTabs))
+    { elem.tabs(scope.$eval(attr.dsTabs))
     }
   }
 
