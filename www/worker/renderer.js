1ba89b3a41d71297bfa07a8bdb1cc7b5755920fe
Jeremy Hughes <jedahu@gmail.com>
2013-01-18 15:05:46 +1300

    renderer: escape using numeric entities

--- www/worker/renderer.js
+++ www/worker/renderer.js
@@ -29,9 +29,11 @@
       }
     , escapeHtml = function(str)
       { return str
-        . replace(/&/, '&amp;')
-        . replace(/</, '&lt;')
-        . replace(/>/, '&gt;')
+        . replace(/&/, '&#38;')
+        . replace(/</, '&#60;')
+        . replace(/>/, '&#62;')
+        . replace(/"/, '&#34;')
+        . replace(/'/, '&#39;')
       }
 
 ; XMLHttpRequest = null
