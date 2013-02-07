497e39a52a9d608087f00de7f5e1760429df4278
Jeremy Hughes <jedahu@gmail.com>
2013-02-08 02:28:30 +1300

    rebuild dist

--- dist/worker/renderer/markdown.js
+++ dist/worker/renderer/markdown.js
@@ -1,57 +1,24 @@
-; importScripts('lib/marked.js')
+; var text = ''
 
-; var lang = ''
-    , text = ''
+; importScripts('lib/evilstreak-markdown.js')
 
-; receiveOnce
-    ( 'lang'
-    , function(str)
-      { lang = str
-      }
-    )
-; receive
-    ( 'comment'
-    , function()
-      { // pass
-      }
-    )
-; receive
-    ( '/comment'
-    , function()
-      { // pass
-      }
-    )
-; receive
-    ( 'code'
-    , function()
-      { text += '\n```' + lang + '\n'
-      }
-    )
-; receive
-    ( '/code'
-    , function()
-      { text += '```\n\n'
-      }
-    )
-; receive
-    ( 'code.line'
-    , function(line)
-      { text += line
-      }
-    )
-; receive
-    ( 'comment.line'
-    , function(line)
-      { text += line.text
-      }
-    )
-; receive
-    ( 'end'
-    , function()
-      { sendHtml(marked.parse(text))
-      ; send('toc', null)
-      ; send('names', null)
-      ; ack('end')
-      ; close()
-      }
-    )
+; receive('comment.open', function(_label)
+    { text = ''
+    })
+
+; receive('comment.line', function(line)
+    { text += line.text
+    })
+
+; receive('comment.close', function(_label)
+    { sendHtml(markdown.toHTML(text, 'Maruku'))
+    })
+
+; receive('html', function(html)
+    { sendHtml(html)
+    })
+
+; receive('end', function()
+    { ack('end')
+    ; close()
+    })
