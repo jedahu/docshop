5de912da6a5342e3ac801674bc3974b324f60b5e
Jeremy Hughes <jedahu@gmail.com>
2013-02-08 22:14:03 +1300

    modify server.js and hooks for nodejitsu deploy

--- server.js
+++ server.js
@@ -14,11 +14,13 @@
         , 'offline.appcache'
         , '404.html'
         ]
+    , grunt = require('grunt')
     , http = require('http')
     , fs = require('fs')
     , path = require('path')
     , zlib = require('zlib')
     , Q = require('q')
+    , fork = require('child_process').fork
     , parseUrl = require('url').parse
     , contentTypes =
         { 'css': ['text/css', 'utf8']
@@ -31,64 +33,69 @@
     , cache = {}
     , zcache = {}
     , sendFile = function(cache, urlPath, response, accept)
-        { var ext = /\.([^.]+)$/.exec(urlPath)[1]
+        { var code = urlPath === '404.html' ? 404 : 200
+            , ext = /\.([^.]+)$/.exec(urlPath)[1]
             , type = contentTypes[ext]
             , gzip = accept && accept.match(/\bgzip\b/) && type[1] && 'gzip'
             , cache = gzip ? zcache : cache
+            , headers = {'Content-Type': type[0]}
         ; urlPath = path.normalize(urlPath)
-        ; response.setHeader('Content-Type', type[0])
-        ; if (gzip) response.setHeader('Content-Encoding', 'gzip')
-        ; cache[urlPath].then(function(val)
+        ; if (gzip) headers['Content-Encoding'] = 'gzip'
+        ; response.writeHead(code, headers)
+        ; response._send('', 'utf8')
+        ; cache[urlPath].promise.then(function(val)
             { response.end(val)
             })
         }
 
 ; files.forEach(function(file)
-    { var data = Q.defer()
-        , zdata = Q.defer()
-    ; cache[file] = data.promise
-    ; zcache[file] = zdata.promise
-    ; fs.readFile('dist/' + file, function(err, val)
-        { if (err)
-            { data.reject(err)
-            }
-          else
-            { data.resolve(val)
-            }
-        })
-    ; data.promise
-        .then(function(val)
-          { zlib.gzip(val, function(err, zval)
-              { if (err)
-                  { zdata.reject(err)
-                  }
-                else
-                  { zdata.resolve(zval)
-                  }
-              })
-          return zdata.promise
-          })
-        .then
-          ( function(zval)
-              {
-              }
-          , function(err)
-              { console.log(err)
-              ; process.exit(1)
-              }
-          )
+    { cache[file] = Q.defer()
+    ; zcache[file] = Q.defer()
+    })
 
+; fork('./grunt', ['all:min']).on('exit', function()
+    { files.forEach(function(file)
+        { var data = cache[file]
+            , zdata = zcache[file]
+        ; fs.readFile('dist/' + file, function(err, val)
+            { if (err)
+                { data.reject(err)
+                }
+              else
+                { data.resolve(val)
+                }
+            })
+        ; data.promise
+            .then(function(val)
+              { zlib.gzip(val, function(err, zval)
+                  { if (err)
+                      { zdata.reject(err)
+                      }
+                    else
+                      { zdata.resolve(zval)
+                      }
+                  })
+              return zdata.promise
+              })
+            .then
+              ( function(zval)
+                  {
+                  }
+              , function(err)
+                  { console.log(err)
+                  ; process.exit(1)
+                  }
+              )
+        })
     })
 
 ; http.createServer(function(request, response)
     { var urlPath = path.normalize(parseUrl(request.url).pathname).slice(1)
     ; if (urlPath && !cache[urlPath] || urlPath === '404.html' || urlPath === 'docs.html')
-        { response.statusCode = 404
-        ; sendFile(cache, '404.html', response, request.headers['accept-encoding'])
+        { sendFile(cache, '404.html', response, request.headers['accept-encoding'])
         }
       else
-        { response.statusCode = 200
-        ; sendFile(cache, urlPath || 'docs.html', response, request.headers['accept-encoding'])
+        { sendFile(cache, urlPath || 'docs.html', response, request.headers['accept-encoding'])
         }
     })
     .listen(process.env.PORT || 5000, '0.0.0.0')
