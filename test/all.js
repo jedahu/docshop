faee72342d924355091884f4e03f3614f021dc53
Jeremy Hughes <jedahu@gmail.com>
2013-02-15 16:22:14 +1300

    move test files into app directory

--- test/all.js
+++
@@ -1,2 +0,0 @@
-; module mp from 'browse/service/manifest_parser.js'
-; module sp from 'browse/service/src_parser/directive.js'
---
+++ www/js/app/test_all.js
@@ -0,0 +1,3 @@
+; module _1 from 'browse/service/manifest_parser_test.js'
+; module _2 from 'browse/service/src_parser/directive_test.js'
+; module _3 from 'browse/service/src_parser_test.js'
