60cbc7b0ef658a79027c8b72322ad78cb420bb3d
Jeremy Hughes <jedahu@gmail.com>
2013-02-15 16:14:08 +1300

    src_parser/directive: document, remove unused code

--- www/js/app/browse/service/src_parser/directive.js
+++ www/js/app/browse/service/src_parser/directive.js
@@ -2,6 +2,7 @@
 title: Built-In Directives
 author: Jeremy Hughes <jedahu@gmail.com>
 tests: /test/browse/service/src_parser/directive.js
+...
 */
 /*
 Built-in directives are prefixed by "!" and all consist of a tag and argument
@@ -10,17 +11,17 @@ argument text is parsed by a function in this module of the same
 name as the tag. Each function takes that single string argument and returns a
 string of HTML.
 
-(Two directives are handled specially by the [parser](src_parser.js) `!meta` and
-`!code`.)
+(Two directives are handled specially by the [parser](../src_parser.js) `!meta`
+and `!code`.)
 */
 ; import escapeHtml from '/util/escape_html.js'
+
+; export module directive
 
 /* !name directive.name (str) => html
 Transforms a string of the form `name free-form-type-information`, into a div
 containing that string and with an id of "name:`name`".
 */
-
-; export module directive
     { export const name = (str) =>
         { const [_, name, info] = /^(\S+)(?:\s+(.+))?$/.exec(str) || []
         ; const infoSpan = info
@@ -34,30 +35,5 @@ containing that string and with an id of "name:`name`".
             + escapeHtml(name)
             + infoSpan
             + '</div>'
-        }
-
-    ; export const meta = (i) =>
-        { for (; i < lines.length; ++i)
-            { if (isCommentClose(lines[i])) break
-            ; metaStr += lines[i] + '\n'
-            }
-        ; me.metaData = jsyaml.load(metaStr)
-        ; return ++i
-        }
-    ; const consumeCodeComment = (label, i) =>
-        { const [_, lang] = /^(\w+)/.exec(str) || []
-        ; const langClass = lang ? 'lang-' + lang : ''
-        ; me.emit
-            ( 'html'
-            , '<pre class="ds:comment-code prettyprint">'
-              + '<code class="'
-              + langClass
-              + '">'
-            )
-        ; for (; i < lines.length; ++i)
-            { if (isCommentClose(lines[i])) break
-            ; me.emit('html', escapeHtml(lines[i] + '\n'))
-            }
-        ; return ++i
         }
     }
